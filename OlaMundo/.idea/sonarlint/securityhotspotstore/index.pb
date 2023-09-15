767aa811324878cfb75fdbf9dfff36ce714b3778
João Victor Oliveira <jvmdeoliveira@gmail.com>
2023-09-14 21:31:23 -0300

    adiciona conteudo da aula 1

---
+++ OlaMundo/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+^
+.src/main/java/devinhouse/novopacote/Hello.java,d\4\d4c8bad0142cd02914b9702c3f6cc1ccaa6b9eb3
+Z
+*src/main/java/devinhouse/clamed/Hello.java,e\0\e01e9b3e056a123577e3df8fb34fdc07f045bb5f
---
+++ OlaMundo/src/main/java/devinhouse/clamed/Hello.java
@@ -0,0 +1,33 @@
+package devinhouse.clamed;
+
+public class Hello {
+     public static void main(String[] args){
+         String nome = "André Ricardo";
+         nome = "true";
+
+         int idade = 30;
+         System.out.println(idade);
+
+         idade = 50;
+
+         char $letra = 'a';
+
+         System.out.println(idade);
+
+         double altura = 1.75;
+         altura = altura + 1;
+         boolean _vascaino = true;
+         System.out.println(altura);
+
+         int id4d3 = 10;
+
+         String nomeCompleto = "João Victor Oliveira";
+         String nomecompleto = "Matheus Ramos";
+
+         System.out.println(nomeCompleto);
+         String character;
+         final String nomeDaMae = "Marta Oliveira";
+     }
+
+     public void outroMetodoDeTeste() {}
+}
